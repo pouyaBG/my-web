4bdfa1c54c80c156dccdafb1509cc271d5bf1563
Pouya Barari Gelyard <93615101+pouyaBG@users.noreply.github.com>
2023-08-20 11:28:11 +0330

    navbar & footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,47 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const handlTopScrool = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+  return (
+    <footer className="w-full flex flex-col text-black-100 mt-5 bg-white">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-4 sm:px-16 px-6 py-5">
+        <Link href={"/"} className="flex items-center">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            width={26}
+            height={19}
+            className="object-contain mr-3"
+          />
+          <span className="self-center text-[15px] font-semibold whitespace-nowrap dark:text-white">
+            Pouya B.G
+          </span>
+        </Link>
+        <p className="text-base text-gray-700">
+          .تمام حقوق اين وب‌سايت متعلق به پویا براری گلیرد است &copy;
+        </p>
+        <button
+          onClick={handlTopScrool}
+          className="flex justify-between items-center border rounded-md p-2"
+        >
+          <Image
+            src={"/arrow-down.svg"}
+            width={15}
+            height={10}
+            priority
+            alt="arrow"
+            className="object-contain rotate-180 mr-1"
+          />
+          <>بازگشت به بالا</>
+        </button>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,18 +2,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-
+import { usePathname } from "next/navigation";
 const Navbar = () => {
   const [iSopne, setIsOpen] = useState(false);
   const [iSopneModal, setIsOpenModal] = useState(false);
+  const currentRoute = usePathname();
+  console.log(currentRoute);
 
   return (
     <header className="w-full ">
       <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="max-w-screen-[1440px] flex flex-wrap items-center justify-between mx-auto p-4">
           <Link href={"/"} className="flex items-center">
             <Image
-              src={"/ds.png"}
+              src={"/logo.png"}
               alt="logo"
               width={39}
               height={19}
@@ -96,6 +98,9 @@ const Navbar = () => {
                   >
                     <li>
                       <Link
+                        onClick={() => {
+                          setIsOpen(false);
+                        }}
                         href="#"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
@@ -104,6 +109,9 @@ const Navbar = () => {
                     </li>
                     <li>
                       <Link
+                        onClick={() => {
+                          setIsOpen(false);
+                        }}
                         href="#"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
@@ -112,6 +120,9 @@ const Navbar = () => {
                     </li>
                     <li>
                       <Link
+                        onClick={() => {
+                          setIsOpen(false);
+                        }}
                         href="#"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
@@ -134,7 +145,9 @@ const Navbar = () => {
               <li>
                 <Link
                   href="/aboutme"
-                  className="block py-2 pl-3 pr-4 font-bold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className={`${
+                    currentRoute === "/aboutme" ? "text-blue-600" : ""
+                  } py-2 pl-3 pr-4 font-bold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
                 >
                   درباره من
                 </Link>
@@ -143,7 +156,9 @@ const Navbar = () => {
               <li>
                 <Link
                   href="/"
-                  className="block py-2 pl-3 pr-4 font-bold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className={`${
+                    currentRoute === "/" ? "text-blue-600" : ""
+                  } py-2 pl-3 pr-4 font-bold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
                 >
                   صفحه اصلی
                 </Link>
